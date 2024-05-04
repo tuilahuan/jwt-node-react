6201093b977ec6aea9fd816ee56e8884d57cdfb7
tuilahuan <vanhuana4k56@gmail.com>
2024-05-04 17:53:21 +0700

    update

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,12 @@
 import express from "express";
+import homeController from "../controller/homeController";
 
 const router = express.Router();
 
 const initWebRouter = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("Hello world");
-  });
+  router.get("/", homeController.handleHelloWorld);
+
+  router.get("/user", homeController.handleUserPage);
 
   return app.use("/", router);
 };
