ec08a01d8354454e7f7ae396bb7d19f73e4b591f
tuilahuan <vanhuana4k56@gmail.com>
2024-06-14 10:20:16 +0700

    create modals table

--- src/models/groupRole.js
+++ src/models/groupRole.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class GroupRole extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -15,17 +15,12 @@ module.exports = (sequelize, DataTypes) => {
   };
 
   //object relational mapping
-  User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    username: DataTypes.STRING,
-    address: DataTypes.STRING,
-    sex: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    groupId: DataTypes.INTEGER
+  GroupRole.init({
+    groupId: DataTypes.INTEGER,
+    roleId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'GroupRole',
   });
-  return User;
+  return GroupRole;
 };
--- src/models/projectUser.js
+++ src/models/projectUser.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class ProjectUser extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -15,17 +15,12 @@ module.exports = (sequelize, DataTypes) => {
   };
 
   //object relational mapping
-  User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    username: DataTypes.STRING,
-    address: DataTypes.STRING,
-    sex: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    groupId: DataTypes.INTEGER
+  ProjectUser.init({
+    projectId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'ProjectUser',
   });
-  return User;
+  return ProjectUser;
 };
